8576cb995e67278572c240dccf1a24996fef46e2
Your Name <kirikarankiri63@gmail.com>
2020-02-12 12:59:27 +0530

    create file upload ui style

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const path=require('path');
 //Init app
 const app=express();
 
+//EJS
+app.set('view engine','ejs');
+
+//public folder
+app.use(express.static('./public'));
+
+app.get('/',(req,res)=> res.render('index'));
+
 const port=3000;
 
 app.listen(port,()=> console.log(`Server started on port ${port}`));
